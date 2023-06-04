e3130412ad3fd22a7367865a98ac844074899a46
Hanbali19 <127258799+Hanbali19@users.noreply.github.com>
2023-06-04 14:30:21 +0100

    Update cachematrix.R
    solved the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  inverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInverse <- function(inv) {
+    inverse <<- inv
+  }
+  
+  getInverse <- function() {
+    inverse
+  }
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  inverse <- x$getInverse()
+  
+  if (!is.null(inverse)) {
+    message("Getting cached result")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data)
+  x$setInverse(inverse)
+  inverse
 }
+
